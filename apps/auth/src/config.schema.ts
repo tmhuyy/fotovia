99f109ee8305e2ee22512e489e8504a0ef8d1c06
huy.tranminh <tmhuyy.itiu@gameloft.com>
2025-09-25 04:50:14 +0700

    [auth]: update config schema

--- apps/auth/src/config.schema.ts
+++ apps/auth/src/config.schema.ts
@@ -4,7 +4,7 @@ export const ConfigSchemaValidation = Joi.object({
   ENV: Joi.string().default('DEV').required(),
   PORT: Joi.number().default(3000).required(),
   DB_HOST: Joi.string().required(),
-  DB_PORT: Joi.string().default('542').required(),
+  DB_PORT: Joi.string().default('5432').required(),
   DB_USERNAME: Joi.string().required(),
   DB_PASSWORD: Joi.string().required(),
   DB_DATABASE: Joi.string().required(),
