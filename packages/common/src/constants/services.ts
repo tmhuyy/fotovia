7b47a0a610260b9127571ae082749578b95143ba
huy.tranminh <tmhuyy.itiu@gmail.com>
2026-03-17 18:10:18 +0700

    feat[common]: update constant

--- packages/common/src/constants/services.ts
+++ packages/common/src/constants/services.ts
@@ -1,1 +1,4 @@
-export const AUTH_SERVICE = 'auth'
+export const AUTH_SERVICE = "auth";
+export const BOOKING_SERVICE = "booking";
+export const PROFILE_SERVICE = "profile";
+export const ASSET_SERVICE = "asset";
